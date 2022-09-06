e858343b3682519dfcc25a2e01118e6a4724ce47
weeeeeeee <95114619+meA-sonWat@users.noreply.github.com>
2022-09-06 14:04:56 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function 等於()
+{
+  let 白癡 = document.getElementById("result").value
+  let 廢物;
+  try {
+    廢物 = eval(白癡)
+  } catch (error) {
+    console.log('oppps, is syntax error now!');
+    alert("oppps, is syntax error now!");
+    return;
+  }
+
+  document.getElementById("result").value = 廢物 
+}
+
+const 彩蛋 = new Audio();
+彩蛋.src = 'audio/quack_5.mp3';
